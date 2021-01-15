9ef82ce75796b4db8add53eecc6e19b1515749de
antocrne <69148589+antocrne@users.noreply.github.com>
2021-01-15 22:00:19 +0100

    overlay content

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     $('#toggle').click(function() {
         $(this).toggleClass('active');
         $('#overlay').toggleClass('open');
+        gsap.to(".ol", {delay: 0.5, opacity: 1, top: 0, duration: 0.5, ease: "power2"});
+        gsap.to(".jp__brand", {delay: 1, opacity: 1, duration: 1});
     });
 }); 
+
 
