b1265de7a60dc701bf49ecde1e83404fceb606c6
antocrne <69148589+antocrne@users.noreply.github.com>
2021-01-16 14:26:47 +0100

    home page content

--- js/script.js
+++ js/script.js
@@ -6,5 +6,51 @@ $('#toggle').click(function() {
 });
 
 
+var controller = new ScrollMagic.Controller();
 
-var rellax = new Rellax('.title__rellax');
+new ScrollMagic.Scene({
+    triggerElement: "#reveal",
+    triggerHook: 0.8,
+    offset: 50 
+})
+.setClassToggle("#reveal", "visible")
+
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#enter1",
+    triggerHook: 0.7, 
+    offset: 50 
+})
+.setClassToggle("#enter1", "visible")
+
+.addTo(controller);
+
+
+new ScrollMagic.Scene({
+    triggerElement: "#reveal2",
+    triggerHook: 0.9, 
+    offset: 50 
+})
+.setClassToggle("#reveal2", "visible") 
+
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#reveal3",
+    triggerHook: 0.9,
+    offset: 50 
+})
+.setClassToggle("#reveal3", "visible") 
+
+.addTo(controller);
+
+
+new ScrollMagic.Scene({
+    triggerElement: "#reveal4",
+    triggerHook: 0.9,
+    offset: 50 
+})
+.setClassToggle("#reveal4", "visible") 
+
+.addTo(controller);
